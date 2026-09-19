7361067bc250ed9ff922e809eb939d36aa8dc2a3
rehanwarraich <rehanwarraich115@gmail.com>
2026-09-19 23:39:29 +0500

    Fix GitHub Pages configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/LearnLite/',
   plugins: [react()],
   resolve: {
     alias: {
